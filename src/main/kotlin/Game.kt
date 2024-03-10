12c574749cf3c6354c217623f30097421c4971d3
James Gadoury <gadouryjames@gmail.com>
2024-03-10 12:45:57 -0400

    Prototype some basic game interactions

--- src/main/kotlin/Game.kt
+++ src/main/kotlin/Game.kt
@@ -1,48 +1,66 @@
 package org.lost
 
-import java.util.*
-
 enum class Action {
     Cooperate, Defect
 }
 
-object AgentFunctions {
-    private val registered = mutableMapOf<Int, () -> Action>()
-    fun register(f: () -> Action, id: Int) {
+data class AgentPlay(val id: String, val action: Action, val reward: Int)
+typealias Round = Pair<AgentPlay, AgentPlay>
+typealias AgentFunction = (String, List<Round>) -> Action
+data class Game(val gameLength: Int, val rounds: List<Round>)
+
+object Agents {
+    private val registered = mutableMapOf<String, AgentFunction>()
+    fun register(id: String, f: AgentFunction) {
         registered[id] = f
     }
 
-    fun get(id: Int) : (() -> Action)?  {
+    fun getAgentFunction(id: String) : AgentFunction?  {
         return registered[id]
     }
-}
 
-data class GameOutcome(val agent1Score: Int, val agent2Score: Int) {}
-
-fun playRound(agent1Id: Int, agent2Id: Int): GameOutcome {
-    val agent1Play = AgentFunctions.get(agent1Id)!!()
-    val agent2Play = AgentFunctions.get(agent2Id)!!()
-
-    return if (agent1Play == Action.Cooperate && agent2Play == Action.Cooperate) {
-        GameOutcome(3, 3)
-    } else if (agent1Play == Action.Defect && agent2Play == Action.Defect) {
-        GameOutcome(1, 1)
-    } else if (agent1Play == Action.Defect) {
-        GameOutcome(5, 0)
-    } else {
-        GameOutcome(0, 5)
+    fun getAgentIds() : List<String> {
+        return registered.keys.toList()
     }
 }
 
-fun playGame(rounds: Int, agent1Id: Int, agent2Id: Int): GameOutcome {
+fun playRound(agent1Id: String, agent2Id: String, priorRounds: List<Round>): Round {
+    val agent1Action = Agents.getAgentFunction(agent1Id)!!(agent1Id, priorRounds)
+    val agent2Action = Agents.getAgentFunction(agent2Id)!!(agent2Id, priorRounds)
+    val (agent1Score, agent2Score) = if (agent1Action == Action.Cooperate && agent2Action == Action.Cooperate) {
+        Pair(3, 3)
+    } else if (agent1Action == Action.Defect && agent2Action == Action.Defect) {
+        Pair(1, 1)
+    } else if (agent1Action == Action.Defect) {
+        Pair(5, 0)
+    } else {
+        Pair(0, 5)
+    }
+    return Round(AgentPlay(agent1Id, agent1Action, agent1Score), AgentPlay(agent2Id, agent2Action, agent2Score))
+}
+
+fun playGame(gameLength: Int, agent1Id: String, agent2Id: String): Game {
+    val rounds = mutableListOf<Round>()
+    for (i in 0..gameLength) {
+       rounds.addLast(playRound(agent1Id, agent2Id, rounds))
+    }
+    return Game(gameLength, rounds)
+}
+
+fun evaluateGame(game: Game) : String {
     var agent1Score = 0
     var agent2Score = 0
-
-    for (i in 0..rounds) {
-        val outcome = playRound(agent1Id, agent2Id)
-        agent1Score += outcome.agent1Score
-        agent2Score += outcome.agent2Score
+    // for now just count all the rounds and see who won or tied
+    for (round in game.rounds) {
+        agent1Score += round.first.reward
+        agent2Score += round.second.reward
     }
 
-    return GameOutcome(agent1Score, agent2Score)
+    return if (agent1Score == agent2Score) {
+        "Tie"
+    } else if (agent1Score > agent2Score) {
+        "${game.rounds.first().first.id} won"
+    } else {
+        "${game.rounds.first().second.id} won"
+    }
 }
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,14 +1,57 @@
 package org.lost
 
-import kotlinx.coroutines.*
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 import redis.clients.jedis.JedisPooled
+import kotlin.random.Random
+
+fun alwaysCooperate(agentId: String, priorRounds: List<Round>) : Action {
+    return Action.Cooperate
+}
+
+fun alwaysDefect(agentId: String, priorRounds: List<Round>) : Action {
+    return Action.Defect
+}
+
+fun randomAction(agentId: String, priorRounds: List<Round>) : Action {
+    return if (Random.nextFloat() > 0.5) Action.Cooperate else Action.Defect
+}
+
+fun titForTat(agentId: String, priorRounds: List<Round>) : Action {
+    if (priorRounds.isEmpty()) return Action.Cooperate
+    val lastRound = priorRounds.last()
+    return if (agentId == lastRound.first.id) lastRound.second.action else lastRound.first.action
+}
+
+fun <T> getCombinations(c: List<T>) : List<Pair<T, T>> {
+    val pairs = mutableListOf<Pair<T, T>>()
+    for (i in 0..<c.size-1) {
+        for (j in i+1..<c.size) {
+            pairs.addLast(Pair<T, T>(c[i], c[j]))
+        }
+    }
+    return pairs
+}
 
 fun main() = runBlocking {
     val jedis = JedisPooled("localhost", 6379)
 
-    for (i in 1..10000) {
+    // generate 3 agents of each type
+    for (i in 0..3) {
+        Agents.register("alwaysCoop$i", ::alwaysCooperate)
+        Agents.register("alwaysDefect$i", ::alwaysDefect)
+        Agents.register("randomAction$i", ::randomAction)
+        Agents.register("titForTat$i", ::titForTat)
+    }
+
+    val combos = getCombinations(Agents.getAgentIds())
+    println("number of games that will be played: ${combos.size}")
+
+    for (i in 0..<combos.size) {
         launch {
-            val status = jedis.set("blah${i}", "${i*2}")
+            val combo = combos[i]
+            val game = playGame(10, combo.first, combo.second)
+            val status = jedis.set("game${i}", evaluateGame(game))
             if ("OK" != status) {
                 println("$i execution had error")
             }
