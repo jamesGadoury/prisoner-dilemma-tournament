d974c8e1be0c517525ef781a34727acfde8d90b2
James Gadoury <gadouryjames@gmail.com>
2024-03-16 16:41:25 -0400

    Add some docstrings for fun

--- src/main/kotlin/lost/Game.kt
+++ src/main/kotlin/lost/Game.kt
@@ -11,6 +11,10 @@ data class AgentResultFromGame(val id: String, val totalScore: Int)
 @Serializable
 data class GameResult(val agent1Result: AgentResultFromGame, val agent2Result: AgentResultFromGame)
 
+/**
+ * Score a round between two agents using the [agent1Action] and [agent2Action].
+ * @return (agent1 score, agent2 score)
+ */
 fun scoreRound(agent1Action: Action, agent2Action: Action): Pair<Int, Int> {
     return if (agent1Action == Action.Cooperate && agent2Action == Action.Cooperate) {
         Pair(3, 3)
@@ -23,6 +27,10 @@ fun scoreRound(agent1Action: Action, agent2Action: Action): Pair<Int, Int> {
     }
 }
 
+/**
+ * Play a round between two agents using the [agent1Id] and [agent2Id] to look up their respective agent functions. [priorRounds] is used as an input into each agent function.
+ * @return data from the played round.
+ */
 fun playRound(agent1Id: String, agent2Id: String, priorRounds: List<Round>): Round {
     val agent1Action = Agents.getAgentFunction(agent1Id)!!(agent1Id, priorRounds)
     val agent2Action = Agents.getAgentFunction(agent2Id)!!(agent2Id, priorRounds)
@@ -30,6 +38,10 @@ fun playRound(agent1Id: String, agent2Id: String, priorRounds: List<Round>): Rou
     return Round(AgentPlay(agent1Id, agent1Action, agent1Score), AgentPlay(agent2Id, agent2Action, agent2Score))
 }
 
+/**
+ * Play a game between two agents using the [agent1Id] and [agent2Id] to look up their respective agent functions. [continueProbability] is used to check if game should continue after each round.
+ * @return data from the played game.
+ */
 fun playGame(agent1Id: String, agent2Id: String, continueProbability: Double): Game {
     val rounds = mutableListOf<Round>()
     while (Random.nextFloat() <= continueProbability) {
@@ -38,6 +50,10 @@ fun playGame(agent1Id: String, agent2Id: String, continueProbability: Double): G
     return Game(agent1Id, agent2Id, rounds)
 }
 
+/**
+ * Evaluate a played [game] to collect information relevant to the game.
+ * @return statistics from the played game.
+ */
 fun evaluateGame(game: Game): GameResult {
     var agent1Score = 0
     var agent2Score = 0
@@ -50,6 +66,11 @@ fun evaluateGame(game: Game): GameResult {
     return GameResult(AgentResultFromGame(game.agent1Id, agent1Score), AgentResultFromGame(game.agent2Id, agent2Score))
 }
 
+/**
+ * Get a sequence iterating through the actions of opponent of agent with id = [selfId] from [priorRounds] in reverse order. [selfId]
+ * should be equal to the agent we want to get the opponent of, not the opponent's id.
+ * @return sequence of opponent's actions in reverse order (starting with last action).
+ */
 fun opponentActionsReversed(selfId: String, priorRounds: List<Round>): Sequence<Action> = sequence {
     for (round in priorRounds.reversed()) {
         yield(if (selfId == round.first.id) round.second.action else round.first.action)
